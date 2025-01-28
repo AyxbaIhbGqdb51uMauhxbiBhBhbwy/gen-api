7788ecb870ed3cff99befee4458425079e8d8024
AyxbaIhbGqdb51uMauhxbiBhBhbwy <cateringbilalonline@gmail.com>
2025-01-29 01:33:11 +0700

    Update api.py

--- api.py
+++ api.py
@@ -29,3 +29,30 @@ def random_account():
             return jsonify({"error": "Invalid account format"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+@app.route('/api/Steam', methods=['GET'])
+def random_account():
+    try:
+        # Membaca file acc.txt
+        with open('accsteam.txt', 'r') as file:
+            accounts = file.readlines()
+        
+        # Memastikan file tidak kosong
+        if not accounts:
+            return jsonify({"error": "No accounts found"}), 404
+        
+        # Memilih satu baris acak
+        random_account = random.choice(accounts).strip()
+        
+        # Memisahkan username dan password
+        if ":" in random_account:
+            username, password = random_account.split(":", 1)
+            return jsonify({
+                           "username": username,
+                "password": password,
+                "combo": random_account
+            }), 200
+        else:
+            return jsonify({"error": "Invalid account format"}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
